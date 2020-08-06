8803dc6435dba367aa6fd6ee24457c346545e97f
wjone <engel0088@gmail.com>
2020-08-06 12:37:39 +0900

    chore: customize scrollbars

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import { Container, Button } from 'react-bootstrap'
+
 import ContactsModal from './components/ContactsModal'
+import CustomScrollbars from './components/CustomScrollbars'
 
 import styles from "./styles/Modal.module.css"
 
 const ModalA = () => {
+  const [contacts, setContacts] = useState(['mack', 'jack', 'collin', 'garett', 'mack', 'jack', 'collin', 'garett', 'mack', 'jack', 'collin', 'garett'])
+  const onReachedToBottom = () => {
+    setContacts([...contacts, 'ABC', '1231', 'sdfsdf', 'DFC', 'ABC', '1231', 'sdfsdf', 'DFC'])
+  }
+
   return (
     <ContactsModal title="All Contacts" isOpen={true}>
-      All Contacts will be here.
+      <CustomScrollbars onReachedBottom={onReachedToBottom} style={{height: 300}}>
+        {contacts.map( (contact, id) => (<p key={id}>{contact}</p>))}
+      </CustomScrollbars>
     </ContactsModal>
   )
 }
---
+++ src/components/CustomScrollbars.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import { Scrollbars } from 'react-custom-scrollbars'
+
+const CustomScrollbars = ({children, onReachedBottom, ...props}) => {
+  const [reachedBottom, setReachedBottom] = useState(false)
+
+  const handleUpdate = ({ scrollTop, scrollHeight, clientHeight }) => {
+    const pad = 100
+    const t = ((scrollTop + pad) / (scrollHeight - clientHeight))
+    if (t > 1) {
+      setReachedBottom(true) 
+    } else {
+      setReachedBottom(false)
+    }
+  }
+
+  useEffect(() => {
+    if(reachedBottom === true) {
+      onReachedBottom()
+    }
+  }, [reachedBottom])
+
+  return (
+    <Scrollbars style={props.style} onUpdate={handleUpdate}>
+      { children }
+    </Scrollbars>
+  )
+}
+
+export default CustomScrollbars
